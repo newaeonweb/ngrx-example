38bad9d34fd017f69f9865da46f1bf0f278c1c5a
newaeonweb <fernando@newaeonweb.com.br>
2019-06-14 11:11:27 -0300

    import order adjust

--- src/app/pages/beer/beer.module.ts
+++ src/app/pages/beer/beer.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { beerRoutes } from './beer.routes';
 
 // Module component
+import { BeerComponent } from './beer.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
 import { BeerDetailComponent } from './beer-detail/beer-detail.component';
 
@@ -12,8 +12,12 @@ import { BeerDetailComponent } from './beer-detail/beer-detail.component';
 import { SearchPipe } from './pipes/search.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
 
+// Module Routes
+import { beerRoutes } from './beer.routes';
+
 @NgModule({
   declarations: [
+    BeerComponent,
     BeerListComponent,
     BeerDetailComponent,
     SearchPipe,
