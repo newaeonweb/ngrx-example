d5d5d10ae0a542e40c5c89d2170d972a3dc61194
newaeonweb <fernando@newaeonweb.com.br>
2019-06-14 16:33:27 -0300

    added new line

--- src/app/app-store/reducers/app.reducer.ts
+++ src/app/app-store/reducers/app.reducer.ts
@@ -1,7 +1,8 @@
 import { ActionReducerMap } from '@ngrx/store';
-
 import { routerReducer } from '@ngrx/router-store';
+
 import { AppState } from '../state/app.state';
+
 import { configReducers } from '../../pages/user/_user-store/config.reducer';
 import { userReducers } from '../../pages/user/_user-store/user.reducer';
 
