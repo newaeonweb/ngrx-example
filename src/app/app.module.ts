9cd660c241b20e0513b1de0a226193938e896275
newaeonweb <fernando@newaeonweb.com.br>
2019-06-14 16:31:11 -0300

    removed uneeded imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 
-// NGRX State managemant modules
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+// App routing module
+import { AppRoutingModule } from './app-routing.module';
 
 // NGRX State managemant imports
 import { AppStoreModule } from './app-store/app-store.module';
-import { appReducers } from './app-store/reducers/app.reducer';
 
 // App modules
 import { BeerModule } from './pages/beer/beer.module';
@@ -19,7 +14,6 @@ import { UserModule } from './pages/user/user.module';
 
 // App components
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
