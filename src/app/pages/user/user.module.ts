bf47373cfa861626fba6ce35c4bc914bb25d50fe
newaeonweb <fernando@newaeonweb.com.br>
2019-06-14 10:45:10 -0300

    renamed modeules

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { UserListComponent as UsersContainerComponent } from './user-list/user-list.component';
-import { UsersComponent } from './components/user-list-component/user-list.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { UserDetailsComponent } from './components/user-details-component/user-details.component';
+// Modules Pages
+import { UserListComponent as UserPageComponent } from './user-list/user-list.component';
+import { UserDetailComponent as UserDetailPageComponent } from './user-detail/user-detail.component';
 
+// Pages components
+import { UserDetailsComponent } from './components/user-details-component/user-details.component';
+import { UsersComponent } from './components/user-list-component/user-list.component';
+
+// Module Routes
 import { userRoutes } from './user.route';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    UsersContainerComponent,
+    UserPageComponent,
+    UserDetailPageComponent,
     UsersComponent,
-    UserDetailComponent,
     UserDetailsComponent,
   ],
   imports: [RouterModule.forChild(userRoutes), CommonModule],
