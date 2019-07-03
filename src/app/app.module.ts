1e7865aff7d4bdf529628b5a8c1f0aca4378b736
newaeonweb <fernando@newaeonweb.com.br>
2019-07-03 14:32:45 -0300

    removed unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppStoreModule } from './app-store/app-store.module';
 
 // App modules
-import { BeerModule } from './pages/beer/beer.module';
+import { AuthModule } from './pages/auth/auth.module';
 import { HomeModule } from './pages/home/home.module';
+import { CartoonModule } from './pages/cartoon/cartoon.module';
 
 // App components
 import { AppComponent } from './app.component';
-import { CartoonModule } from './pages/cartoon/cartoon.module';
-import { AuthModule } from './pages/auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,7 +24,6 @@ import { AuthModule } from './pages/auth/auth.module';
     HttpClientModule,
     AppStoreModule,
     AuthModule,
-    BeerModule,
     HomeModule,
     CartoonModule,
   ],
