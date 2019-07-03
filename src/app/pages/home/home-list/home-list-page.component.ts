ca3d5799e40ac40f2b6bd2ef3c1b9b5af245dc9a
newaeonweb <fernando@newaeonweb.com.br>
2019-07-03 14:58:17 -0300

    small css adjust

--- src/app/pages/home/home-list/home-list-page.component.ts
+++ src/app/pages/home/home-list/home-list-page.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
           <div class="row is-center">
             <div class="card is-text-center">
               <header>
-                <h4>
+                <h1>
                   <a routerLink="/signin">THE RICK AND MORTY</a>
-                </h4>
+                </h1>
               </header>
               <img
                 class="img-fluid"
                 src="./assets/images/rkmt.jpeg"
                 style="height:250px!important; margin: 0 auto; border-radius: 5px;"
               />
-              <p class="bg-primary text-white">
+              <p class="bg-primary text-white lead">
                 NGRX Example using state management and simple authentication
               </p>
               <footer class="is-center">
@@ -31,7 +31,9 @@ import { Component, OnInit } from '@angular/core';
       </div>
     </div>
   `,
-  styles: ['.card { background-color: #fff9ed; width: 60%; }'],
+  styles: [
+    '.card { background-color: #fff9ed; width: 60%; } .lead { font-size: 1.2em; font-weight: 200}',
+  ],
 })
 export class HomeListPageComponent implements OnInit {
   constructor() {}
